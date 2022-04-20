071fe45fe48994628fcdaac6e903303c216f0437
ZinchenkoAY <AZinchenko@qulix.com>
2022-04-20 11:04:11 +0300

    by with count and delete

--- src/main/java/com/belnarto/spring/data/repository/AccountRepository.java
+++ src/main/java/com/belnarto/spring/data/repository/AccountRepository.java
@@ -3,7 +3,15 @@ package com.belnarto.spring.data.repository;
 import com.belnarto.spring.data.entity.AccountEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountRepository extends CrudRepository<AccountEntity, UUID> {
+
+    long countByUser(String user);
+
+    long deleteByBalance(BigDecimal balance);
+
+    List<AccountEntity> removeByBalance(BigDecimal balance);
 }
